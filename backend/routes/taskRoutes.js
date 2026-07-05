e4a7e6bd6b87ffb3e8dfc820127dd6cd4ca84ee8
SanduniNimasha <159423773+SanduniNimasha@users.noreply.github.com>
2026-07-06 01:08:54 +0530

    Update taskRoutes.js

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const router = express.Router();
 
+const {
+    getAllTasks,
+    getTaskById,
+    createTask,
+    updateTask,
+    deleteTask,
+    updateStatus,
+    getDashboard
+} = require("../controllers/taskController");
+
+router.get("/", getAllTasks);
+router.get("/dashboard", getDashboard);
+router.get("/:id", getTaskById);
+router.post("/", createTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
+router.patch("/:id/status", updateStatus);
+
+module.exports = router;
