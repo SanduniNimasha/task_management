4e5530368c152bad7ddf99b3504bcdadf300a608
SanduniNimasha <159423773+SanduniNimasha@users.noreply.github.com>
2026-07-06 01:05:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const taskRoutes = require("./routes/taskRoutes");
+
+app.use("/api/tasks", taskRoutes);
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
